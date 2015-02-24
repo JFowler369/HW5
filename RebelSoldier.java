71a1663622610ab9b8313fc1ca386a1694f44761
JFowler369 <jessefowler@ymail.com>
2015-02-24 16:29:42 -0500

    Added RebelSoldier and Stormtrooper Methods

--- RebelSoldier.java
+++ RebelSoldier.java
@@ -1,5 +1,19 @@
 public class RebelSoldier extends Soldier {
 
+    public RebelSoldier(double health, double attack,
+            double defense, String identifier) {
+        super(health, attack, defense, identifier)
+    }
 
+    @override
+    public String getName() {
+        return "Rebel Soldier " + this.getIdentifier();
+    }
+
+    public double attack(Soldier target) {
+        if (math.random(100) >= 20) {
+            target.hurt(this.getAttack())
+        }
+    }
 
 }
--- Stormtrooper.java
+++ Stormtrooper.java
@@ -1,5 +1,14 @@
 public class Stormtrooper extends Soldier {
 
+    @override
+    public String getName() {
+        return "Stormtrooper " + this.getIdentifier();
+    }
 
+    public double attack(Soldier target) {
+        if (math.random(100) >= 40) {
+            target.hurt(this.getAttack() * 1.25)
+        }
+    }
 
 }
