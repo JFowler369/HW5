3555b9cbe574767affe65385533e09885159c747
JFowler369 <jessefowler@ymail.com>
2015-02-24 17:12:16 -0500

    Added Sith class

---
+++ Sith.java
@@ -0,0 +1,32 @@
+public abstract class Sith extends Soldier implements ForceSensitive {
+
+    private double power;
+    private boolean hasUsedForce = false;
+
+    public Sith(double health, double attack,
+            double defense, String identifier) {
+        super(health, attack, defense, identifier);
+        this.power = (attack + defense) / 6;
+    }
+
+    public double attack(Soldier target) {
+        target.hurt(this.getAttack());
+        if (hasUsedForce) {
+            this.changeAttack(-1 * power);
+        }
+        this.hasUsedForce = false;
+        return this.getAttack();
+    }
+
+    public void useTheForce() {
+        this.hurt(power / 10);
+        this.changeAttack(power);
+        this.hasUsedForce = true;
+    }
+
+    @Override
+    public String getName() {
+        return "Sith " + this.getIdentifier();
+    }
+
+}
