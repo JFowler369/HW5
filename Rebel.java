883fa318c90a48885a09faf569e0eced29c1a8e8
JFowler369 <jessefowler@ymail.com>
2015-02-24 17:20:26 -0500

    Added custome Troops

---
+++ Rebel.java
@@ -0,0 +1,8 @@
+abstract class Rebel extends RebelSoldier {
+
+  public Rebel(double health, double attack,
+          double defense, String identifier) {
+      super(health, attack, defense, identifier);
+  }
+
+}
---
+++ Trooper.java
@@ -0,0 +1,8 @@
+public abstract class Trooper extends Stormtrooper {
+
+    public Trooper(double health, double attack,
+            double defense, String identifier) {
+        super(health, attack, defense, identifier);
+    }
+
+}
